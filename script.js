4be464f4007d018876ba8491778e89b78436f306
Elioenai Pinheiro Ferrari <elioenai.ferrari@ifood.com.br>
2024-10-14 17:09:43 -0300

    add monthly investment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /**
  * @typedef {Object} Req
  * @property {number} investment
- * @property {number} monthly_income
+ * @property {number} monthlyIncome
  * @property {number} months
  */
 
@@ -15,37 +15,47 @@
 /**
  * @typedef {Object} Res
  * @property {Prediction[]} predictions
- * @property {number} total_dividend
+ * @property {number} totalDividend
+ * @property {number} totalMonthlyInvestment
  * @property {number} total
  */
 
 /**
  * Calculate investment predictions.
  *
- * @param {number} investment - Initial investment amount.
- * @param {number} monthly_income - Monthly income from the investment.
+ * @param {number} initialInvestment - Initial investment amount.
+ * @param {number} monthlyInvestment - Monthly investment amount.
+ * @param {number} monthlyIncome - Monthly income from the investment.
  * @param {number} months - Number of months to calculate.
  * @returns {Res} The result containing predictions, total dividend, and total amount.
  */
-function calculateInvestment(investment, monthly_income, months) {
+function calculateInvestment(
+  initialInvestment,
+  monthlyInvestment,
+  monthlyIncome,
+  months
+) {
   const res = {
     predictions: [],
-    total_dividend: 0.0,
+    totalDividend: 0.0,
     total: 0.0,
   };
 
   for (let i = 0; i < months; i++) {
-    const dividend = monthly_income * (investment + res.total_dividend);
-    res.total_dividend += dividend;
+    const dividend = monthlyIncome * (initialInvestment + res.totalDividend);
+    res.totalDividend += dividend;
 
     const prediction = {
       month: i + 1,
       dividend: dividend,
-      total: investment + res.total_dividend,
+      monthlyInvestment: monthlyInvestment,
+      total: initialInvestment + res.totalDividend + monthlyInvestment,
     };
     res.predictions.push(prediction);
   }
 
-  res.total = investment + res.total_dividend;
+  res.total =
+    initialInvestment + res.totalDividend + monthlyInvestment * months;
+  res.totalMonthlyInvestment = monthlyInvestment * months;
   return res;
 }
